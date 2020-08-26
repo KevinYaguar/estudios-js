9dfd8f918210f543a354d0f165a2aae9d66d112f
KevinYaguar <68433099+KevinYaguar@users.noreply.github.com>
2020-08-26 13:31:13 -0300

    ejercicio edades

--- clase-25-8.js
+++ clase-25-8.js
@@ -11,12 +11,48 @@ do{
 alert('Bienvenido ' + usuarioIngresado);
 */
 
+/* SUMA Y PROMEDIO
 var suma = 0;
+var cantidadMax = 5;
 
-for (var i = 1; 1 <= 5; i++){
+for (var i = 1; i <= cantidadMax; i++){
     var numeroIngresado = prompt('Igrese numero')
     suma = suma + parseInt(numeroIngresado);
 }
+ alert('los numeros ingresados suman ' + suma)
+ alert('el promedio de los numeros ingresador es de ' + suma/cantidadMax);
+*/
+
+// EJERCICIO 3: EDADES
+/*
+variable suma 
+i = canidad de veces
+
+ingrese edad
+es la edad 0?
+No, suma + edad ingresada
+ingrese otra edad 
+es la edad 0? 
+No, suma + edad ingresada
+ingrese otra edad 
+es la edad 0? 
+si
+rompe ciclo y calcula
+*/
+var edadIngresada; //undefinded
+var mayoresDeEdad = 0;
+
+do{
+    edadIngresada = parseInt(prompt('ingrese edad'));
+
+    if(edadIngresada >= 18){
+        mayoresDeEdad++; 
+    }
+    
+    
+}while(edadIngresada!==0)
+
+alert(' El total de mayores  es  ' + mayoresDeEdad);
 
 
 

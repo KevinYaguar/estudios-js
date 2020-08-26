5a981e1b104917be6ea1b0622812dcaa2d0479d1
KevinYaguar <68433099+KevinYaguar@users.noreply.github.com>
2020-08-25 23:48:40 -0300

    clase 25-8

---
+++ clase-25-8.js
@@ -0,0 +1,22 @@
+/*
+const USUARIO = "Alejandro";
+const CONTRASEÑA = "123456";
+
+do{
+    alert('le vamos a solicitar su usario y contraseña')
+    var usuarioIngresado = prompt('ingrese su suario');
+    var contraseñaIngresado = prompt('ingrese contraseña');
+}while(usuarioIngresado !== USUARIO || contraseñaIngresado !== CONTRASEÑA)
+
+alert('Bienvenido ' + usuarioIngresado);
+*/
+
+var suma = 0;
+
+for (var i = 1; 1 <= 5; i++){
+    var numeroIngresado = prompt('Igrese numero')
+    suma = suma + parseInt(numeroIngresado);
+}
+
+
+
